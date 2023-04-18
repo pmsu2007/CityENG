cce319a2b070f3787ed88a9d2fbdac2af79063be
pmsu2007 <pmsu2008@gmail.com>
2023-04-18 17:55:50 +0900

    URL 수정

--- src/config/key.js
+++ src/config/key.js
@@ -1,3 +1,3 @@
-// export const APIURL = "http://192.168.0.3:8080"; // 집
-//export const APIURL = "http://172.20.10.6:8080"; // 핫스팟
-export const APIURL = "http://172.17.96.130:8080"; // 핫스팟
+// export const APIURL = "http://192.168.0.3:8080";
+// export const APIURL = "http://172.20.10.6:8080";
+export const APIURL = "";
